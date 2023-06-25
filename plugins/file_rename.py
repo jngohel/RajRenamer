eedd6d030aa7107f42fad050d7192dd82373c8a3
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-06-25 21:27:08 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.enums import MessageMediaType
 from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
-
+from pyrogram.file_id import FileId
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
