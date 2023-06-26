48a9ad58a189065cf088c22d2a821f08570584b8
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-06-26 14:12:23 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters 
 from helper.database import db
-from config import ADMIN
+from config import Config, ADMIN
 
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
