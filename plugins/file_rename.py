fb7677cce2e7067de3531fc19b039f438875cf19
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-08-05 15:49:57 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -29,7 +29,6 @@ async def rename_start(client, message):
             await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
             await sleep(30)
         except FloodWait as e:
-            await sleep(e.value)
             text = f"""<b>ᴡʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴍᴇ ᴛᴏ ᴅᴏ ᴡɪᴛʜ ᴛʜɪs ꜰɪʟᴇ??\n\nꜰɪʟᴇ ɴᴀᴍᴇ - <code>{filename}</code>\n\nꜰɪʟᴇ sɪᴢᴇ - <code>{filesize}</code>\n\nᴅᴄ ɪᴅ - <code>{dcid}</code></b>"""
             buttons = [[InlineKeyboardButton("ʀᴇɴᴀᴍᴇ", callback_data="rename"),
 	    	        InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="cancel")]]
