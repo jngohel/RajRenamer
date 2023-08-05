6a53d72c5978c8441cb71739da5fba6edb179aa6
Rishikesh Sharma(Rk_botz) <118276768+Rishi09090909@users.noreply.github.com>
2023-08-05 22:31:20 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -53,6 +53,11 @@ class Database:
         user = await self.col.find_one({'_id': int(id)})
         return user.get('caption', None)
 
+
+    async def get_user(self, user_id):
+        user_data = await self.users.find_one({"id": user_id})
+        return user_data
+
     async def update_user(self, user_data):
         await self.users.update_one({"id": user_data["id"]}, {"$set": user_data}, upsert=True)
 
