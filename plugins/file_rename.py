1a27c526c294e483579e10edc3a681a1bf6ae31f
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-08-05 21:24:13 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -33,9 +33,7 @@ async def rename_start(client, message):
             buttons = [[InlineKeyboardButton("ʀᴇɴᴀᴍᴇ", callback_data="rename"),
 	    	        InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="cancel")]]
             await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
-        else:
-	    await message.reply(text="only for paid users")
-		
+
 @Client.on_callback_query(filters.regex('rename'))
 async def rename(bot, update):
 	user_id = update.message.chat.id
