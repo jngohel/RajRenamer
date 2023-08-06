f432fd446e4c9b31493e8e842898922f0b62ed06
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-08-06 14:32:26 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -17,17 +17,18 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot, message):
     aksid = message.from_user.id
-    if await not db.has_premium_access(aksid): return
-        content = message.text
-        user = message.from_user.first_name
-        user_id = message.from_user.id
-        if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
-        if user_id in Config.ADMIN: return # ignore admins
-        await message.reply_text("<b>Yᴏᴜʀ ᴍᴇssᴀɢᴇ ʜᴀs ʙᴇᴇɴ sᴇɴᴛ ᴛᴏ ᴍʏ ᴍᴏᴅᴇʀᴀᴛᴏʀs !</b>")
-        await bot.send_message(
-	    chat_id=Config.LOG_CHANNEL,
-            text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
-        )
+    if await not db.has_premium_access(aksid): 
+        return
+    content = message.text
+    user = message.from_user.first_name
+    user_id = message.from_user.id
+    if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
+    if user_id in Config.ADMIN: return # ignore admins
+    await message.reply_text("<b>Yᴏᴜʀ ᴍᴇssᴀɢᴇ ʜᴀs ʙᴇᴇɴ sᴇɴᴛ ᴛᴏ ᴍʏ ᴍᴏᴅᴇʀᴀᴛᴏʀs !</b>")
+    await bot.send_message(
+	chat_id=Config.LOG_CHANNEL,
+        text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
+     )
 
 
 @Client.on_message(filters.private & (filters.document | filters.audio | filters.video))
