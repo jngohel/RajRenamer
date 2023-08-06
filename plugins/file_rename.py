59a6927812fcc3244c9c861eda8df9df6aea35bf
Sahidmalik001 <106477157+Sahidmalik001@users.noreply.github.com>
2023-08-06 11:26:30 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -36,7 +36,8 @@ async def rename_start(client, message):
 	    	        InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="cancel")]]
             await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
     else:
-	await message.reply_text("only for paid users")
+        await message.reply_text("only for paid users")	
+
     		
         
         
