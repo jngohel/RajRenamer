a05abc85855fce38934a01a988078853de6f3370
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-08-09 18:08:37 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -44,10 +44,6 @@ async def rename_start(client, message):
                      InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="cancel")]
                 ]
                 await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
-		await client.send_message(
-		    chat_id=Config.LOG_CHANNEL,
-		    text=f"<b>User - {aks}\n\nUser id - {aksid}\n\nFile Name - {filename}\n\nFile Size - {filesize}\n\nDC ID - {dcid}</b>"
-		)
     else:
         if message.media:
             file = getattr(message, message.media.value)
