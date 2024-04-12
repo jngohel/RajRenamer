b6b5197b9bc72403c605ee6b8e486be5fc676680
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-04-12 18:54:11 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -1,31 +1,6 @@
 from pyrogram import Client, filters 
 from helper.database import db
-
-@Client.on_message(filters.private & filters.command('set_caption'))
-async def add_caption(client, message):
-    if len(message.command) == 1:
-       return await message.reply_text("<b>ɢɪᴠᴇ ᴍᴇ ᴄᴀᴘᴛɪᴏɴ ʟɪᴋᴇ 👇\n\n<code>/set_caption 📝 ɴᴀᴍᴇ - {filename}\n\n📏 sɪᴢᴇ - {filesize}\n\n🕒 ᴅᴜʀᴀᴛɪᴏɴ - {duration}</code></b>")
-    caption = message.text.split(" ", 1)[1]
-    await db.set_caption(message.from_user.id, caption=caption)
-    await message.reply_text("<b>sᴜᴄᴄᴇssꜰᴜʟʟʏ ᴄᴀᴘᴛɪᴏɴ sᴀᴠᴇᴅ ⚡️</b>")
-   
-@Client.on_message(filters.private & filters.command('del_caption'))
-async def delete_caption(client, message):
-    caption = await db.get_caption(message.from_user.id)  
-    if not caption:
-       return await message.reply_text("**ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴀᴘᴛɪᴏɴ 🚫**")
-    await db.set_caption(message.from_user.id, caption=None)
-    await message.reply_text("**ᴄᴀᴘᴛɪᴏɴ ᴅᴇʟᴇᴛᴇᴅ 🌬**")
                                        
-@Client.on_message(filters.private & filters.command(['see_caption', 'view_caption']))
-async def see_caption(client, message):
-    caption = await db.get_caption(message.from_user.id)  
-    if caption:
-       await message.reply_text(f"**ʏᴏᴜʀ ᴄᴀᴘᴛɪᴏɴ -**\n\n`{caption}`")
-    else:
-       await message.reply_text("**ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴀᴘᴛɪᴏɴ ‼️**")
-
-
 @Client.on_message(filters.private & filters.command(['view_thumb', 'viewthumb']))
 async def viewthumb(client, message):    
     thumb = await db.get_thumbnail(message.from_user.id)
