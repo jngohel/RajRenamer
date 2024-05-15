949aac97a77378675e8411556b679e69f2497d6c
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-05-15 19:23:51 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -18,43 +18,43 @@ class Database:
             caption=None
         )
 
-    async def add_user(b, m):
+    async def add_user(self, b, m):
         u = m.from_user
         if not await is_user_exist(u.id):
             user = new_user(u.id)
             usrcol.insert_one(user)            
             await send_log(b, u)
 
-    async def is_user_exist(id):
+    async def is_user_exist(self, id):
         user = usrcol.find_one({'_id': int(id)})
         return bool(user)
 
-    async def total_users_count():
+    async def total_users_count(self):
         count = usrcol.count_documents({})
         return count
 
-    async def get_all_users():
+    async def get_all_users(self):
         all_users = usrcol.find({})
         return all_users
 
-    async def delete_user(user_id):
+    async def delete_user(self, user_id):
         usrcol.delete_many({'_id': int(user_id)})
     
-    async def set_thumbnail(id, file_id):
+    async def set_thumbnail(self, id, file_id):
         usrcol.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
 
-    async def get_thumbnail(id):
+    async def get_thumbnail(self, id):
         user = usrcol.find_one({'_id': int(id)})
         return user.get('file_id', None)
 
-    async def get_user(user_id):
+    async def get_user(self, user_id):
         user_data = premium.find_one({"id": user_id})
         return user_data
 
-    async def update_user(user_data):
+    async def update_user(self, user_data):
         premium.update_one({"id": user_data["id"]}, {"$set": user_data}, upsert=True)
 
-    async def has_premium_access(user_id):
+    async def has_premium_access(self, user_id):
         user_data = await get_user(user_id)
         if user_data:
             expiry_time = user_data.get("expiry_time")
@@ -66,10 +66,10 @@ class Database:
                 premium.update_one({"id": user_id}, {"$set": {"expiry_time": None}})
         return False
     
-    async def update_user(user_data):
+    async def update_user(self, user_data):
         await premium.update_one({"id": user_data["id"]}, {"$set": user_data}, upsert=True)
 
-    async def update_one(filter_query, update_data):
+    async def update_one(self, filter_query, update_data):
         try:
             result = premium.update_one(filter_query, update_data)
             return result.matched_count == 1
@@ -77,14 +77,14 @@ class Database:
             print(f"Error updating document: {e}")
             return False
 
-    async def get_expired(current_time):
+    async def get_expired(self, current_time):
         expired_users = []
         if data := premium.find({"expiry_time": {"$lt": current_time}}):
             async for user in data:
                 expired_users.append(user)
         return expired_users
 
-    async def remove_premium_access(user_id):
+    async def remove_premium_access(self, user_id):
         return await premium.update_one(
             {"id": user_id}, {"$set": {"expiry_time": None}}
             )
