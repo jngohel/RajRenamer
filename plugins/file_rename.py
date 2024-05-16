030ceb477061347ae3b1227d63e73c396e9e924b
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-05-16 13:53:36 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -86,7 +86,8 @@ async def rename_and_upload(bot, message: Message, thumbnail_file_id, new_filena
 @Client.on_message(filters.channel & filters.chat(source_channel_id))
 async def auto_rename_and_forward(client, message):
     try:
-        thumbnail_file_id = await db.get_thumbnail()  # Get the thumbnail file ID from the database
+	chat_id = message.chat.id
+        thumbnail_file_id = await db.get_thumbnail(chat_id)
         if not thumbnail_file_id:
             await message.reply_text("No thumbnail found in the database.")
             return
