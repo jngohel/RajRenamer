328fc3e19e1c6b0453e1bc199fdd2bf449d7f498
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-05-18 15:12:19 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -85,8 +85,8 @@ async def rename_and_upload(bot, message: Message, thumbnail_file_id, new_filena
 
 @Client.on_message(filters.chat(source_channel_id) & (filters.document | filters.video))
 async def file_received(client, message):
-    thumbnail_file_id = await db.get_thumbnail(message.from_user.id)
-    message.from_user = message.from_user
+    user_id = message.from_user.id if message.from_user else 0
+    thumbnail_file_id = await db.get_thumbnail(user_id)
     await rename_and_upload(client, message, thumbnail_file_id)
 
 @Client.on_message(filters.private & filters.command(["batch"]))
